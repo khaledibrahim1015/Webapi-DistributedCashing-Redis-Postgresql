3fc9e7a91a649c7a972fb0a4f0096f4319916291
KhaledIbrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-21 20:56:49 +0300

    Install Packages and Depen. => Create DbContext

--- DistributedCashing-redis/DistributedCashingredisWebapi/Data/AppDbContext.cs
+++ DistributedCashing-redis/DistributedCashingredisWebapi/Data/AppDbContext.cs
@@ -1,2 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace DistributedCashingredisWebapi;
 
+public class AppDbContext : DbContext 
+{
+    public DbSet<Driver> Drivers {set;get;}
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base (options){}
+    
+
+
+
+}
