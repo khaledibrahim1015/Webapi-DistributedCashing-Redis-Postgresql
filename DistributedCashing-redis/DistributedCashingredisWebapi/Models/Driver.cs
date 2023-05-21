2aba218a5b20bbf532d1d3c32ded33a8ae3b4680
KhaledIbrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-21 20:45:29 +0300

    add models

---
+++ DistributedCashing-redis/DistributedCashingredisWebapi/Models/Driver.cs
@@ -0,0 +1,10 @@
+namespace DistributedCashingredisWebapi;
+
+public class Driver 
+{
+
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int DriverNumber { get; set; }
+
+}
